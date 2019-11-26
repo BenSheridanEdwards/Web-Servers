a2a55a226b6b182ba52e87ce85d47e6b78e687b8
bensheridanedwards <bensheridanedwards@gmail.com>
2019-11-26 16:05:44 +0000

    Add get /cat route, and link it to a cat photo

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'shotgun'
 
-gets '/' do 
-  'hello!'
+get '/' do 
+  'Hello Ben!'
+end
+
+get '/cat' do
+"<div> 
+ <img src='http://bit.ly/1eze8aE'> 
+ </div>"
+end
+
+get '/secret' do
+  'Tiss the season...to be jolly'
 end
